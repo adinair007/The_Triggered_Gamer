f1d26b0dd92227ef9c398bf28f2485b25e850d30
Aditya Nair <adinair.in@gmail.com>
2023-01-10 21:00:11 -0600

    Created auth.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const gameName = document.querySelector('#game-title').value.trim();
+  const review = document.querySelector('#game-review').value.trim();
 
-  if (name && needed_funding && description) {
+  if (gameName && review) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ gameName, review }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -17,7 +16,7 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create review');
     }
   }
 };
@@ -33,7 +32,7 @@ const delButtonHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to delete project');
+      alert('Unable to delete this review.');
     }
   }
 };

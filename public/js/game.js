ae7689d1de4fd0bec5f032edc92e4362cf8cad32
Aditya Nair <adinair.in@gmail.com>
2023-01-12 15:45:50 -0600

    fixed error in game

--- public/js/game.js
+++ public/js/game.js
@@ -1,36 +1,31 @@
 const searchForm = async (event) => {
-    event.preventDefault();
-  
+  event.preventDefault();
+
   const reviewForm = async (event) => {
     event.preventDefault();
-  
-    console.log("Inside reviewForm");
-    let ratingEl = document.getElementById("inputRating");
-    let reviewContent = document.getElementById("userReview");
-    let gameTitle = document.getElementById("game-title")
-  
+
+    console.log('Inside reviewForm');
+    let ratingEl = document.getElementById('inputRating');
+    let reviewContent = document.getElementById('userReview');
+    let gameTitle = document.getElementById('game-title');
+
     // console.log(ratingEl.value);
     if (!ratingEl.value) {
-      alert("Please fill in the rating.")
+      alert('Please fill in the rating.');
     }
     // text content || verify its filled in
     if (!reviewContent.value) {
-      alert("Please fill in the review.")
+      alert('Please fill in the review.');
     }
-    // game slug || 
+    // game slug ||
     gameTitle = gameTitle.textContent;
     if (!gameTitle) {
-      alert("Please search the game you want to review.")
+      alert('Please search the game you want to review.');
     }
-  
-    const postReview = {
-  
-    }
-  
-  
-  
-  }
-  
-  
-  document.getElementById("reviewForm").addEventListener("submit", reviewForm);
-  document.querySelector(".searchForm").addEventListener("submit", searchForm);
+
+    const postReview = {};
+  };
+};
+
+document.getElementById('reviewForm').addEventListener('submit', reviewForm);
+document.querySelector('.searchForm').addEventListener('submit', searchForm);

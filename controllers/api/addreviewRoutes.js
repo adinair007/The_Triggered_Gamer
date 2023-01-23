01c727c1238b8416c97954b763e469795177591e
Aditya Nair <adinair.in@gmail.com>
2023-01-22 20:31:35 -0600

    connect to jawsdb

--- controllers/api/addreviewRoutes.js
+++ controllers/api/addreviewRoutes.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const { Review } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+// router.post('/', withAuth, async (req, res) => {
+  router.post('/', async (req, res) => {
   try {
     const newReview = await Review.create({
       ...req.body,

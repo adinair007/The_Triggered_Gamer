ca27fbf89d3eb20cade0d40d5f5d63236a93b180
Aditya Nair <adinair.in@gmail.com>
2023-01-22 20:11:20 -0600

    test

--- controllers/api/addreviewRoutes.js
+++ controllers/api/addreviewRoutes.js
@@ -1,17 +1,25 @@
 const router = require('express').Router();
 const { Review } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
     const newReview = await Review.create({
-      review_body: req.body.review_body,
-      review_date: req.body.review_date,
-      // user_id: req.session.user_id,
+      ...req.body,
+      user_id: req.session.user_id,
     });
 
-    req.status(200).json(newReview);
+    // const newReview = await Review.create
+    // ({
+    //   game_title: req.body.game_title,
+    //   review: req.body.review,
+    //   user_score: req.body.user_score,
+    //   review_date: req.body.date_created,
+    //   user_id: req.session.user_id,
+    // });
+    res.status(200).json(newReview);
   } catch (err) {
-    res.status(500).json(error);
+    res.status(500).json(err);
   }
 });
 
@@ -33,7 +41,7 @@ router.put('/:id', withAuth, async (req, res) => {
 
     res.status(200).json(review);
   } catch (err) {
-    res.status(500).json(error);
+    res.status(500).json(err);
   }
 });
 
@@ -53,7 +61,7 @@ router.delete('/:id', withAuth, async (req, res) => {
 
     res.status(200).json({ success: true });
   } catch (err) {
-    res.status(500).json(error);
+    res.status(500).json(err);
   }
 });
 

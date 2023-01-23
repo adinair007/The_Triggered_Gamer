220458a6774ee485abaf8b24904b00f5274c1abb
Aditya Nair <adinair.in@gmail.com>
2023-01-23 12:44:52 -0600

    Few bug fixes

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,7 +3,7 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/reviews/${id}`, {
+    const response = await fetch(`/review/${id}`, {
       method: 'DELETE',
     });
 
